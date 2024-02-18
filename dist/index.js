6ff9e230912d8417ca1ad9fff936ddfc7f162967
Govind <you@example.com>
2024-02-18 18:18:25 +0530

    starting_microservices

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// index.ts
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+ 
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}); 
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
