acb1b222aff19996300828dc8bec62ea024a28b8
Govind <you@example.com>
2024-02-18 19:37:04 +0530

    updating-socketio

---
+++ client/index.js
@@ -0,0 +1,18 @@
+const io = require("socket.io-client");
+
+const socket = io("http://localhost:3001");
+
+socket.on("connect", () => {
+  console.log("Connected to server");
+});
+socket.on("message", (message) => {
+  console.log("message aaya h bhai log", message);
+});
+socket.on("disconnect", () => {
+  console.log("Disconnected from server");
+});
+const msg = "hey how are you govind";
+function sendMsg() {
+  socket.emit("message-bejte", msg);
+}
+sendMsg();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
- 
-import express from 'express';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-}); 
+io.on("connection", (socket) => {
+  console.log("new user is connected", socket.id);
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+  socket.on("message-bejte", (message) => {
+    console.log("message aara hai", message);
+    io.emit("message",message);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("server is started at port 3001");
 });
